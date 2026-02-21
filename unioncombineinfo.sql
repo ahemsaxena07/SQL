9e83e309570e62345a414f261d97cfc76f10cb33
AHEM SAXENA <99779094+ahemsaxena07@users.noreply.github.com>
2026-02-21 22:37:50 +0530

    last progress
    adding union combine information

---
+++ unioncombineinfo.sql
@@ -0,0 +1,33 @@
+--orders are stored in separate tables(orders & orderArchive). Combine all orders into one report without duplicates
+
+SELECT 
+'orders' as SourceTable
+      ,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM sales.Orders
+UNION
+SELECT
+'OrdersArchive' as SourceTable
+      ,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM sales.OrdersArchive
